57a61aa62b26fee2ada8dcf5c7dd08d384bf1212
Pierre <pierrefraisse90@gmail.com>
2023-05-04 23:01:44 +0200

    logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleSubstract = () => {
+    setCount(count - 1);
+  }
+  const handleReset = () => {
+    setCount(0);
+  }
+  const handleAdd = () => {
+    setCount(count + 1);
+  }
+
+  let color = "#fff";
+  if (count >=1) {
+    color = "green";
+  } else if (count < 0) {
+    color = "red";
+  } else {
+    color = "white";
+  }
+
   return (
     <section className="counter-sec --flex-center">
       <div className="container counter --card --center-all">
         <h1 className="--text-light">React Counter App</h1>
-        <p className="count --my2 --text-md --text-light">0</p>
+        <p className="count --my2 --text-md --text-light --fw-bold" style={{color: color}}>{ count }</p>
         <div className="buttons">
-          <button className="--btn --btn-danger">- Substract</button>
-          <button className="--btn">Reset</button>
-          <button className="--btn --btn-success">+ Add</button>
+          <button className="--btn --btn-danger" onClick={handleSubstract}>- Substract</button>
+          <button className="--btn" onClick={handleReset}>Reset</button>
+          <button className="--btn --btn-success" onClick={handleAdd}>+ Add</button>
         </div>
 
       </div>
