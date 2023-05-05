30619919670a2d94c775bf68f258b5df1dce6849
Pierre <pierrefraisse90@gmail.com>
2023-05-05 11:24:56 +0200

    Random audio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0);
+
   const handleSubstract = () => {
     setCount(count - 1);
   }
@@ -13,10 +14,38 @@ function App() {
   const handleAdd = () => {
     setCount(count + 1);
   }
+  
   const handleLucky = () => {
-    const randomNum = Math.floor(Math.random() * (100 - (-100) +1)) + (-100);
-    setCount(randomNum);
+    const context = new AudioContext();
+    const frequency = getRandomFrequency();
+    
+    // Create an oscillator node with the randomly generated frequency
+    const oscillator = context.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.setValueAtTime(frequency, context.currentTime);
+  
+    // Create a gain node to control the volume
+    const gainNode = context.createGain();
+    gainNode.gain.setValueAtTime(1, context.currentTime);
+  
+    // Connect the oscillator to the gain node and the gain node to the context's destination
+    oscillator.connect(gainNode);
+    gainNode.connect(context.destination);
+  
+    // Start the oscillator and schedule it to stop after 0.5 seconds
+    oscillator.start();
+    oscillator.stop(context.currentTime + 0.5);
+  
+    // Set the count state to the randomly generated frequency
+    setCount(frequency);
+  };
+  
+  function getRandomFrequency() {
+    return Math.floor(Math.random() * 900) + 100;
   }
+  
+  
+
 
   let color = "#fff";
   if (count >=1) {
